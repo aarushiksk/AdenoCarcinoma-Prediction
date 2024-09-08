b3c29faa2570461f624449b379bf1c5a5cd896ae
aarushiksk <aarushee002@gmail.com>
2024-09-08 12:05:03 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # from src.ChestCancerClassifier import logger
+
 from ChestCancerClassifier import logger
 from ChestCancerClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChestCancerClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -9,7 +11,46 @@ try:
         obj=DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>>>>> stage{STAGE_NAME} completed <<<<<<<<<<\n \n=======x")
-        
+
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+# STAGE_NAME = "Prepare base model"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    prepare_base_model = PrepareBaseModelTrainingPipeline()
+#    prepare_base_model.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+
+
+# STAGE_NAME = "Training"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_trainer = ModelTrainingPipeline()
+#    model_trainer.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+# STAGE_NAME = "Evaluation stage"
+# try:
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_evalution = EvaluationPipeline()
+#    model_evalution.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
--- src/ChestCancerClassifier/entity/config_entity.py
+++ src/ChestCancerClassifier/entity/config_entity.py
@@ -9,3 +9,17 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
     
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
